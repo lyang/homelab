517f62c0b5a0712542d9081f136fb4ad99ff854c
Lin Yang <coding@lyang.dev>
2025-03-18 23:43:53 -0700

    Bootstrap talos cluster using terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "telmate/proxmox"
       version = "3.0.1-rc6"
     }
+    talos = {
+      source  = "siderolabs/talos"
+      version = "0.7.1"
+    }
   }
 }
 
@@ -17,7 +21,20 @@ provider "proxmox" {
   pm_api_token_secret = var.pve.token-secret
 }
 
+provider "talos" {}
+
 module "pve" {
   source   = "./modules/pve"
   talos-vm = var.talos-vm
+}
+
+module "talos" {
+  source = "./modules/talos"
+  talos-cluster = {
+    name     = "homelab"
+    endpoint = format("https://%s:6443", element(values(module.pve.talos-nodes), 0))
+    nodes = {
+      controlplanes = module.pve.talos-nodes
+    }
+  }
 }
---
+++ terraform/modules/talos/variables.tf
@@ -0,0 +1,10 @@
+variable "talos-cluster" {
+  type = object({
+    name     = string
+    endpoint = string
+    nodes = object({
+      controlplanes = map(string)
+      workers      = optional(map(string), {})
+    })
+  })
+}
